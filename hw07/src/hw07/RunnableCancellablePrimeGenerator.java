33d2199efa6db8d98790147f997fc00ecfc211ea
Atharva Sachin Khadgi <69451310+slashback00@users.noreply.github.com>
2023-12-17 22:56:44 -0500

    Add files via upload

---
+++ hw07/src/hw07/RunnableCancellablePrimeGenerator.java
@@ -0,0 +1,54 @@
+package hw07;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class RunnableCancellablePrimeGenerator extends RunnablePrimeGenerator {
+    private final ReentrantLock lock = new ReentrantLock();
+    private boolean done = false;
+
+    public RunnableCancellablePrimeGenerator(long from, long to) {
+        super(from, to);
+    }
+
+    public void setDone(){
+        lock.lock();
+        try {
+            done = true;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void generatePrimes(){
+        for (long n = from; n <= to; n++) {
+            lock.lock();
+            boolean isDone;
+            try {
+                isDone = done;
+            } finally {
+                lock.unlock();
+            }
+            if(isDone){
+                System.out.println("Stopped generating prime numbers.");
+                this.primes.clear();
+                break;
+            }
+            if(isPrime(n)){ this.primes.add(n); }
+        }
+    }
+	public static void main(String[] args) {
+		RunnableCancellablePrimeGenerator gen = new RunnableCancellablePrimeGenerator(1,100);
+		Thread thread = new Thread(gen);
+		thread.start();
+		gen.setDone();
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		gen.getPrimes().forEach( (Long prime)-> System.out.print(prime + ", ") );
+		System.out.println("\n" + gen.getPrimes().size() + " prime numbers are found.");
+	}
+   
+}
+
